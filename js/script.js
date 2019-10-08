2fb1cc0592cd8dc3600bf044a0ea3f533e059303
Remlix <jachan12@s.sfusd.edu>
2019-10-08 01:09:19 +0000

    started on arrays and splitting

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 $( "document" ).ready(function() {
 $("#button").click(function(){
   let UserInput = $("input").val();
-  
+
+    //let txt = "UserInput"
+    //txt split ("");
+  console.log(UserInput);
+  let words = UserInput.split(" ")
+  console.log(words);
+anArray.forEach(function(UserInput) {
+
 });
 
 
